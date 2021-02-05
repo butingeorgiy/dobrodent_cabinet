781349fd07e1a81ce242f730b8f871e36bf8c7f3
butingeorgiy <butingeorgiy48@yandex.kz>
2021-02-05 22:18:44 +0600

    v1.0.1-dev

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,4 +1,3 @@
-import '../css/app.css';
 import './modules/PatientAuthorizationModule/index';
 import './modules/PatientRegistrationModule/index';
 import './modules/AdministratorAuthorizationModule/index';
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,10 @@
 const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    // mode: 'development',
     entry: './resources/js/index.js',
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'js/app.js'
-    },
-    watch: true,
-    watchOptions: {
-        ignored: /node_modules/,
-        poll: 1000
     },
     module: {
         rules: [
@@ -22,21 +15,7 @@ module.exports = {
                     {loader: "babel-loader"},
                     "source-map-loader"
                 ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    "style-loader",
-                    {loader: "css-loader", options: {importLoaders: 1}},
-                    "postcss-loader",
-                ],
             }
         ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "./resoures/css/app.css",
-            chunkFilename: "./public/css/app.css"
-        })
-    ]
+    }
 };
